1ebc587a6832d7d81770887946fb79f0d8b5601a
fahimabir8 <fahimabir8@users.noreply.github.com>
2025-04-06 22:03:13 +0600

    alarm clock go

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime;
+
+        try{
+            System.out.print("Enter an alarm time (HH:MM:SS): ");
+            String inputTime = scan.nextLine();
+
+            alarmTime = LocalTime.parse(inputTime, formatter);
+            System.out.println("Alarm set for " + alarmTime);
+        }
+        catch (DateTimeParseException e){
+            System.out.println("Invalid format. Please use HH:MM:SS");
+        }
+
+
+
+        scan.close();
+    }
+}
